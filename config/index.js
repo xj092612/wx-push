831c382e72500c4d491385c2fbfe4756b748753d
xj092612 <95918669+xj092612@users.noreply.github.com>
2023-02-23 13:49:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxf6c9b747c09c7522",
+  appsecret: "32152b5e913c9cf9ecfb05ae5463bc31",
+  template_id: "oWO9XXaf6b2xMIPo6IzjYG2jat803o6WeUeH0QR0gN0",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
+  key: "o7TFw6Un23YWqialNC5A3H0uULTU",
   city: "邢台",
   first_date: "2022-11-10",
 };
